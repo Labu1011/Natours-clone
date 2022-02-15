16176a59a8e32090774eda6e4b553f0ff6cad030
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2022-02-15 20:30:47 +0600

    Handling Database Errors as Operational

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
@@ -12,11 +14,19 @@ if (process.env.NODE_ENV === 'development') {
 // body parser middleware
 app.use(express.json())
 
+console.log(process.env.NODE_ENV)
 // static files
 app.use(express.static(`${__dirname}/public`))
 
 // Route middleware
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
+})
+
+// ERROR HANDLING MIDDLEWARE (GLOBAL)
+app.use(globalErrorHandler)
 
 module.exports = app
