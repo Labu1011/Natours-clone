d50294a104410cd1d7634a5edf978bab309dee23
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2022-02-16 23:15:02 +0600

    Creating user with encrypted password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const userController = require('../controllers/userController')
+const authController = require('../controllers/authController')
 
 const router = express.Router()
 
 // User routes
+
+router.post('/signup', authController.signup)
+
 router
   .route('/')
   .get(userController.getAllUsers)
