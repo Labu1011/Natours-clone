7b595e3dc8f6d35a54c632ecbe2b6a0305f86545
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2022-02-23 11:19:00 +0600

    Rate limiting and secure http headers using helmet

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
@@ -7,14 +10,29 @@ const userRouter = require('./routes/userRoutes')
 
 const app = express()
 
+// Global middlewares
+
+// Set security HTTP headers
+app.use(helmet())
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-// body parser middleware
-app.use(express.json())
+// Rate limiting
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!',
+})
 
-// static files
+app.use('/api', limiter)
+
+// body parser middleware
+app.use(express.json({ limit: '10kb' }))
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`))
 
 // Route middleware
