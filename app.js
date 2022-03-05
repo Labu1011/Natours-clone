c3b84fc67b2e5ea0930cbb589e3336d35a0a9687
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2022-03-05 12:09:57 +0600

    Virtual populate reviews on tour

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
+const reviewRouter = require('./routes/reviewRoutes')
 
 const app = express()
 
@@ -32,12 +36,33 @@ app.use('/api', limiter)
 // body parser middleware
 app.use(express.json({ limit: '10kb' }))
 
+// Data sanitization agains noSQL query injection
+app.use(mongoSanitize())
+
+// Data sanitization against XSS
+app.use(xss())
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+)
+
 // Serving static files
 app.use(express.static(`${__dirname}/public`))
 
 // Route middleware
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+app.use('/api/v1/reviews', reviewRouter)
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
